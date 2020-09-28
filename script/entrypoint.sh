720d79df6198048577b97add016f280d5433157b
bli735 <li.brian735@gmail.com>
2020-09-27 22:35:27 -0700

    entrypoint.sh

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,15 +2,23 @@
 
 
 : "${AIRFLOW_HOME:="/usr/local/airflow"}"
+: "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
 
 export \
   AIRFLOW_HOME \
+  AIRFLOW__CORE__EXECUTOR \
 
 case "$1" in
   webserver)
     airflow initdb
-    airflow scheduler
+    if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ] || [ "$AIRFLOW__CORE__EXECUTOR" = "SequentialExecutor" ]; then
+      airflow scheduler &
+    fi
     exec airflow webserver
+    ;;
+  worker|scheduler)
+    sleep 10
+    exec airflow "$@"
     ;;
   *)
     exec "$@"
